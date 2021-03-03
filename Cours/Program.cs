c4cc12253bba091eb80e873e2b557e58fbf198f0
ihab abadi <ihab@utopios.net>
2021-03-03 09:02:52 +0100

    cours AdoNet

---
+++ Cours/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CoursAdoNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connexion à la base de données
+            
+        }
+    }
+}

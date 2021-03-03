22e007f98eb803a38b6eba74d2c293e1853ce65d
ihab abadi <ihab@utopios.net>
2021-03-03 10:51:49 +0100

    cours ado.net partie 3

---
+++ Cours/Classes/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CoursAdoNet.Classes
+{
+    public class Person
+    {
+        private int id;
+        private string fristName;
+        private string lastName;
+        
+
+        public int Id { get => id; set => id = value; }
+        public string FristName { get => fristName; set => fristName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+
+        //public bool Save()
+        //{
+        //    return false;
+        //}
+
+        //public bool Update()
+        //{
+        //    return false;
+        //}
+
+        //public bool Delete()
+        //{
+        //    return false;
+        //}
+
+        //public static List<Person> GetPersons()
+        //{
+        //    return null;
+        //}
+
+        //public static Person GetPersonById(int id)
+        //{
+        //    return null;
+        //}
+    }
+}
---
+++ Cours/Repositories/PersonRepository.cs
@@ -0,0 +1,45 @@
+﻿using CoursAdoNet.Classes;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace CoursAdoNet.Repositories
+{
+    public class PersonRepository
+    {
+        private SqlCommand command;
+        private SqlConnection _connection;
+        private SqlDataReader reader;
+
+        public PersonRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+        public Person Create(Person person)
+        {
+            //insertion dans la base de données
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            //insertion dans la base de données
+            return person;
+        }
+        public Person Delete(Person person)
+        {
+            //insertion dans la base de données
+            return person;
+        }
+        public List<Person> FindAll()
+        {            
+            return null;
+        }
+
+        public Person FindById(int id)
+        {
+            return null;
+        }
+    }
+}

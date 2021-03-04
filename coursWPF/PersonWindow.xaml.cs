e0fbea3ae0885109bb29fb600f0e1ffd2bfb5c7e
ihab abadi <ihab@utopios.net>
2021-03-04 10:11:36 +0100

    exemple cours wpf

---
+++ coursWPF/PersonWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using CoursAdoNet.Classes;
+using CoursAdoNet.Repositories;
+using CoursAdoNet.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace coursWPF
+{
+    /// <summary>
+    /// Logique d'interaction pour PersonWindow.xaml
+    /// </summary>
+    public partial class PersonWindow : Window
+    {
+        public PersonWindow()
+        {
+            InitializeComponent();
+            confirmButton.Click += ConfirmButtonClick;
+        }
+
+        public void ConfirmButtonClick(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person()
+            {
+                FristName = firstNameTextBox.Text,
+                LastName = lastNameTextBox.Text
+            };
+            PersonRepository personRepository = new PersonRepository(Connection.GetSqlConnection());
+            personRepository.Create(person);
+            if(person.Id > 0)
+            {
+                resultTextBlock.Text = $"Personne ajoutée avec l'id {person.Id}";
+            } 
+            else
+            {
+                resultTextBlock.Text = $"Erreur d'insertion dans la base";
+            }
+        }
+    }
+}

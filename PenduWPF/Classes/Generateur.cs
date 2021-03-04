fd89b6c8f1d30fa16c9156f7dbd51286574d50c8
ihab abadi <ihab@utopios.net>
2021-03-04 16:44:20 +0100

    correction pendu partie 1

---
+++ PenduWPF/Classes/Generateur.cs
@@ -0,0 +1,16 @@
+﻿using PenduWPF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PenduWPF.Classes
+{
+    class Generateur : IGenerateur
+    {
+        public string Generer()
+        {
+            //Rechercher aleatoirement un mot à partir d'une table mots
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PenduWPF/Interfaces/IGenerateur.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PenduWPF.Interfaces
+{
+    public interface IGenerateur
+    {
+        string Generer();
+    }
+}

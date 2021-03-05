f1daad41665da35cb4c472f033e816a7dbb3ad92
ihab abadi <ihab@utopios.net>
2021-03-05 09:21:53 +0100

    correction pendu

--- PenduWPF/Classes/Mot.cs
+++ PenduWPF/Classes/Mot.cs
@@ -1,34 +1,32 @@
-﻿using PenduWPF.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
 
 namespace PenduWPF.Classes
 {
-    class Generateur : IGenerateur
+    public class Mot
     {
+        private int id;
+        private string chaine;
         private SqlConnection connection;
         private SqlCommand command;
-        private SqlDataReader reader;
         private string request;
-        public string Generer()
+
+        public int Id { get => id; set => id = value; }
+        public string Chaine { get => chaine; set => chaine = value; }
+
+        public bool Save()
         {
-            //Rechercher aleatoirement un mot à partir d'une table mots
-            string response = null;
-            request = "SELECT TOP 1 mot from mots order by newid()";
+            request = "INSERT INTO mots (mot) output inserted.id values(@mot)";
             connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ihab\source\repos\CoursAP2019\basededonnees.mdf;Integrated Security=True;Connect Timeout=30");
             command = new SqlCommand(request, connection);
+            command.Parameters.Add(new SqlParameter("@mot", Chaine));
             connection.Open();
-            reader = command.ExecuteReader();
-            if(reader.Read())
-            {
-                response = reader.GetString(0);
-            }
-            reader.Close();
+            Id = (int)command.ExecuteScalar();
             command.Dispose();
             connection.Close();
-            return response;
+            return Id > 0;
         }
     }
 }
--- PenduWPF/MainWindow.xaml.cs
+++ PenduWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PenduWPF.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,87 @@ namespace PenduWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        JeuPendu jeu;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ClickJouer(object sender, RoutedEventArgs e)
+        {
+            Generateur g = new Generateur();
+            jeu = new JeuPendu(g);
+            jeu.GenererMasque();
+            if(jeu.Masque != null)
+            {
+                masqueTextBlock.Text = jeu.Masque;
+                nbEssaiTextBlock.Text = "Nombre Essai : " + jeu.NbreEssai;
+                GenererBoutons();
+            }
+        } 
+        private void ClickAjouterMot(object sender, RoutedEventArgs e)
+        {
+            if(motTextBox.Text != "")
+            {
+                Mot mot = new Mot()
+                {
+                    Chaine = motTextBox.Text
+                };
+                if(mot.Save())
+                {
+                    MessageBox.Show("mot ajouté");
+                    motTextBox.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Erreur insertion");
+                }
+            }
+        }
+
+
+        private void GenererBoutons()
+        {
+            boutonsLettres.Children.Clear();
+            for(char c='a'; c <= 'z'; c++)
+            {
+                Button b = new Button()
+                {
+                    Content = c,
+                    Width = 100,
+                    Height = 100,
+                    HorizontalContentAlignment = HorizontalAlignment.Center,
+                    VerticalContentAlignment = VerticalAlignment.Center
+                };
+                b.Click += ClickLettreBouton;
+                boutonsLettres.Children.Add(b);
+            }
+        }
+
+        private void ClickLettreBouton(object sender, RoutedEventArgs e)
+        {
+            if(sender is Button b)
+            {
+                char c = Convert.ToChar(b.Content);
+                if (jeu.TestChar(c))
+                {
+                    masqueTextBlock.Text = jeu.Masque;
+                    if(jeu.TestWin())
+                    {
+                        MessageBox.Show("Bravo !!!!!");
+                    }                    
+                }
+                else
+                {
+                    nbEssaiTextBlock.Text = "Nombre Essai : " + jeu.NbreEssai;
+                    if (jeu.NbreEssai == 0)
+                    {
+                        MessageBox.Show("Perdu !!!!");
+                    }
+                }
+                
+                b.IsEnabled = false;
+            }
         }
     }
 }
